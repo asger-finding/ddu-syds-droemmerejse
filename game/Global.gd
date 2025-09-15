e9ee81dec37495ec37cdaa289ae06600e64d0fd3
Asger Finding <asger.finding@gmail.com>
2025-09-15 09:42:21 +0200

    feat: setup Global

--- game/Global.gd
+++ game/Global.gd
@@ -1,1 +1,6 @@
 extends Node
+
+# Here we initialize our globals
+var GameController
+var Inventory # TODO
+var Player # TODO
--- game/Scripts/GameController.gd
+++ game/Scripts/GameController.gd
@@ -5,15 +5,21 @@ extends Node
 
 var level_instance
 
-func unload_level():
+func _ready():
+	# Remember to set as global
+	Global.GameController = self
+
+func unload_level() -> void:
 	if (is_instance_id_valid(level_instance)):
 		level_instance.queue_free()
 	level_instance = null
 	
-func load_level(level_name: String):
+func load_level(level_name: String) -> void:
 	unload_level()
 	var path := 'res://Levels/%s.tscn' % level_name
 	var level_resource = load(path)
 	if (level_resource):
 		level_instance = level_resource.instance()
 		game.add_child(level_instance)
+	else:
+		printerr('Level path not found')
