cf6d65bcce11f9bde3868af0411f0b3bbd730c83
Lueegee1 <siriusvoss@gmail.com>
2025-09-17 13:06:19 +0200

    Feat: Bad punch, only works one way

--- game/Scripts/Player.gd
+++ game/Scripts/Player.gd
@@ -152,7 +152,15 @@ func _handle_punch():
 		is_rolling=false
 		is_punching = true
 		_animated_sprite.play("Punch")
+		var direction
+		if _animated_sprite.flip_h: 
+			direction = 1 
+		else:
+			direction = -1
 		_punch_hit_box.disabled = false
+		_punch_hit_box.position= position + Vector2(180*direction,70)
+		print(position,_punch_hit_box.position )
+		
 		print("PUNCH!")
 
 
