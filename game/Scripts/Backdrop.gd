fc4b897ae9a7d945a8a25f5428e1518c06ff059b
Asger Finding <asger.finding@gmail.com>
2025-09-22 14:57:07 +0200

    refactor: backdrop changes

--- game/Scripts/Backdrop.gd
+++ game/Scripts/Backdrop.gd
@@ -4,12 +4,17 @@ extends ColorRect
 @export var parallax_speed: float = 0.2  # How fast the background moves relative to camera
 
 var shader_material: ShaderMaterial
-var original_size
+var original_size: Vector2
+var original_zoom: float
+var original_paper_scale: float
 
 func _ready():
-	original_size = size
 	assert(material is ShaderMaterial, "Node needs a ShaderMaterial with the crumpled paper shader")
-	shader_material = material as ShaderMaterial
+	shader_material = material
+
+	original_size = size
+	original_zoom = camera_reference.zoom.x
+	original_paper_scale = shader_material.get_shader_parameter("paper_scale")
 
 func _process(_delta):
 	if camera_reference and shader_material:
@@ -19,3 +24,5 @@ func _process(_delta):
 		position = camera_pos - size / 2
 		
 		shader_material.set_shader_parameter("camera_offset", camera_pos * parallax_speed)
+		# FIXME: needs to shift xy in shader to work
+		# shader_material.set_shader_parameter("paper_scale", 1 / camera_reference.zoom.x * original_zoom * original_paper_scale)
